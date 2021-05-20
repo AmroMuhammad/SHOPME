e06bd7b6dd46a31d6ef269576f25ce67c33c9047
Amro <amrmu94@gmail.com>
2021-05-19 23:44:18 -0700

    add project structure, network layer and utils

---
+++ Shopify e-commerce/Utils/Constants.swift
@@ -0,0 +1,32 @@
+//
+//  Constants.swift
+//  Sportify
+//
+//
+//  Copyright © 2021 ITI-41. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    // MARK: Ahmed Section
+    
+    //end
+    
+    // MARK: Amr Section
+        static let genericError = "error in networkLayer"
+
+    //end
+    
+    
+    // MARK: Ayman Section
+    
+    //end
+    
+    
+    // MARK: Marwa Section
+    
+    //end
+    
+}
+	
