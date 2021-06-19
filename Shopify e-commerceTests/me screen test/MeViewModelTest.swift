47308024a65ce64926fe9a489e6aef4f64390e45
AymanOmara <75618602+AymanOmara@users.noreply.github.com>
2021-06-19 23:46:07 +0200

    me screen unit test

--- Shopify e-commerceTests/me screen test/MeViewModelTest.swift
+++ Shopify e-commerceTests/me screen test/MeViewModelTest.swift
@@ -1,5 +1,5 @@
 //
-//  MEScreenTest.swift
+//  MeViewModelTest.swift
 //  Shopify e-commerceTests
 //
 //  Created by Ayman Omara on 19/06/2021.
@@ -7,22 +7,31 @@
 //
 
 import XCTest
-
-class MEScreenTest: XCTestCase {
-
+@testable import Shopify_e_commerce
+class MeViewModelTest: XCTestCase {
+    var meViewModel:MeViewModel!
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        meViewModel = MeViewModel()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        meViewModel = nil
     }
 
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
+    func testValidation() throws {
+        
+        XCTAssertTrue(meViewModel.emailRegexCheck(text: "ayman@yahoo.com"))
+        XCTAssertFalse(meViewModel.emailRegexCheck(text: "ayman"))
+        
+        
+    }
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
